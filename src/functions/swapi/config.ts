f62cc86c76673d17d863ad876b3a1ee3c7d1491c
lizbethTabraj <lizbeth.tabraj@gmail.com>
2024-04-05 14:23:32 -0500

    fix secret key

--- src/functions/swapi/config.ts
+++ src/functions/swapi/config.ts
@@ -6,8 +6,8 @@ export const BD = {
   },
   //seteare los valores en duro pero deberian ir en el serveless.ts para que sean jalados como parametros desde aws
   aws_remote_config: {
-    accessKeyId: 'AKIAQNBN2EE65LW2BCAW',
-    secretAccessKey: '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',
+    accessKeyId: '------',
+    secretAccessKey: '-',
     region: 'us-east-2',
   }
 };
