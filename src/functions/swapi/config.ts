9ff77f8a1596d4ab2840ac92b4fc904c6fd183cb
lizbethTabraj <lizbeth.tabraj@gmail.com>
2024-04-05 14:31:42 -0500

    fix bd

--- src/functions/swapi/config.ts
+++ src/functions/swapi/config.ts
@@ -1,15 +1,15 @@
 export const BD = {
-  aws_table_name: 'star-wars-people',
+ /* aws_table_name: 'star-wars-people',
   aws_local_config: {
     //Provide details for local configuration
     
   },
   //seteare los valores en duro pero deberian ir en el serveless.ts para que sean jalados como parametros desde aws
-  aws_remote_config: {
+  /*aws_remote_config: {
     accessKeyId: '------',
     secretAccessKey: '-',
     region: '---',
-  }
+  }*/
 };
 
 
